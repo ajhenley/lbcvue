7ebebcb2893e83031ee6957f0247fbec15aaea00
AJ Henley <ajhenley@gmail.com>
2019-10-15 18:35:01 -0400

    working on next lesson

--- main.js
+++ main.js
@@ -1,38 +1,88 @@
-var product = "Shoes";
-var app = new Vue({
-    el: '#app',
-    data: {
-        product: "Shoes",
-        image: "assets/blackshoes.jpg",
-        altText: "A pair of socks",
-        inStock: true,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "black",
-                variantImage: "assets/blackshoes.jpg"
-            },
-            {
-                variantId: 2235,
-                variantColor: "brown",
-                variantImage: "assets/brownshoes.jpg"
-            },
-            {
-                variantId: 2236,
-                variantColor: "red",
-                variantImage: "assets/redshoes.jpg"
-            }
-        ],
-        cart: 0
+Vue.component('product', {
+    template: `
+        <div class="product">
+            <div class="product-image">
+                <img v-bind:src="image" v-bind:alt="altText" />
+            </div>
+            <div class="product-info">
+                <h1>{{ product }}</h1>
+                <p v-if="inStock">In Stock</p>
+                <p v-else="inStock">Out of Stock</p>
+                <p>Shipping: {{ shipping }}</p>
+                
+                <ul>
+                    <li v-for="detail in details">{{ detail }}</li>
+                </ul>
 
+                <div class="color-box"
+                     v-for="variant in variants" :key="variant.variantId"
+                     :style="{ backgroundColor: variant.variantColor }"
+                     @mouseover="updateProduct(variant.variantImage)">
+                </div>
+
+                <button v-on:click="addToCart"
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock }">Add to cart</button>
+
+                <div class="cart">
+                    <p>Cart({{ cart}})</p>
+                </div>
+            </div>
+
+        </div>
+    `,
+    data() {
+        return {
+            product: "Shoes",
+            brand: "Vue AJ",
+            selectedVariant: 0,
+            details: ["80% cotton", "20% polyester", "Gender-neutral"],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "black",
+                    variantImage: "assets/blackshoes.jpg",
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "brown",
+                    variantImage: "assets/brownshoes.jpg",
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2236,
+                    variantColor: "red",
+                    variantImage: "assets/redshoes.jpg",
+                    variantQuantity: 0
+                }
+            ],
+            cart: 0
+
+        }
     },
     methods: {
         addToCart(){
             this.cart += 1
         },
-        updateProduct(variantImage){
-            this.image = variantImage
+        updateProduct(index){
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this]
         }
     }
 })
+var app = new Vue({
+    el: '#app'
+})
+
