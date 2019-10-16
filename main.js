35be28036c1ba6946f9cec987451171d58ac7e6f
AJ Henley <ajhenley@gmail.com>
2019-10-15 21:59:19 -0400

    fixed pobs

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+
     template: `
         <div class="product">
             <div class="product-image">
-                <img v-bind:src="image" v-bind:alt="altText" />
+                <img v-bind:src="image"  />
             </div>
             <div class="product-info">
                 <h1>{{ product }}</h1>
                 <p v-if="inStock">In Stock</p>
                 <p v-else="inStock">Out of Stock</p>
                 <p>Shipping: {{ shipping }}</p>
-                
+                <p>User is premium: {{ premium }}</p>
                 <ul>
                     <li v-for="detail in details">{{ detail }}</li>
                 </ul>
 
                 <div class="color-box"
-                     v-for="variant in variants" :key="variant.variantId"
+                     v-for="(variant, index) in variants" :key="variant.variantId"
                      :style="{ backgroundColor: variant.variantColor }"
-                     @mouseover="updateProduct(variant.variantImage)">
+                     @mouseover="updateProduct(index)">
                 </div>
 
                 <button v-on:click="addToCart"
@@ -78,7 +85,13 @@ Vue.component('product', {
             return this.variants[this.selectedVariant].variantImage
         },
         inStock(){
-            return this.variants[this]
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free"
+            }
+            return 2.99
         }
     }
 })
